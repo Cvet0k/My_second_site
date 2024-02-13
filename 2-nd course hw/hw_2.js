f2080da22986ff65dc39d044fc10f3535681105f
Svetlana Shoronova <svetlanashoronova@gmail.com>
2024-02-13 23:28:59 +0200

    Задания 4 5 и 6

--- 2-nd course hw/hw_2.js
+++ 2-nd course hw/hw_2.js
@@ -6,6 +6,33 @@ alert(20);
 
 //2
 let firstIphonDate = Number(2007);
-alert(`Год выпуска первого iPhone - ${firstIphonDate}`)
+alert(`Год выпуска первого iPhone - ${firstIphonDate}`);
 
+//3
 
+let nameJS = String('Брендан Эйх');
+alert(`Имя создателя языка JavaScript - ${nameJS}`);
+
+// 4
+let k = Number(10);
+let h = Number(2);
+let summ = k + h;
+let subtr = k - h;
+let mult = k * h;
+let divis = k / h;
+alert(`${k} + ${h} = ${summ} 
+${k} - ${h} = ${subtr}
+${k} * ${h} = ${mult}
+${k} / ${h} = ${divis}`);
+
+//5
+let v = Number(2);
+let result = v ** 5;
+alert(result);
+
+//6
+let aa = Number(9);
+let b = Number(2);
+alert(aa % b);
+
+//7
