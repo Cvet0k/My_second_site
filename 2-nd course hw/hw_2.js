988a3f919d5d9e61f7d4263f582cb2f0be937191
Svetlana Shoronova <svetlanashoronova@gmail.com>
2024-02-13 22:38:27 +0200

    Задание 2

--- 2-nd course hw/hw_2.js
+++ 2-nd course hw/hw_2.js
@@ -3,7 +3,9 @@ let a = 10;
 alert(a);
 a = 20;
 alert(20);
-//1_2
-let a = Number(10);
-alert(a);
-alert(a + 10);
+
+//2
+let firstIphonDate = Number(2007);
+alert(`Год выпуска первого iPhone - ${firstIphonDate}`)
+
+
