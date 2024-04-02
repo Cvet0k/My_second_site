8e55409c06381a840d133d02e9fb16c2225e5fcf
Svetlana Shoronova <svetlanashoronova@gmail.com>
2024-04-03 00:21:45 +0300

    task_2

--- 2-nd course hw/hw_6.js
+++ 2-nd course hw/hw_6.js
@@ -1,7 +1,18 @@
 // Задача 1 task
 
-const massiv1 = [ 1, 4, 5, 100, 10, 3];
+const massiv1 = [ 1, 5, 4, 10, 0, 3];
 for (let index = 0; index < massiv1.length; index++) {
      console.log(massiv1[index]);
      if (massiv1[index] === 10) break;
 }
+
+// Задача 2 task
+
+let userIndex = -1
+for (let index = 0; index < massiv1.length; index++) {
+    if (massiv1[index] === 4) {
+        userIndex = index;
+        break ;
+    } 
+}
+console.log(userIndex);
