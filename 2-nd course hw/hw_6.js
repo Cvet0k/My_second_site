a3514d88e19fbe0436b3fce5e35c4b448c4bff7d
Svetlana Shoronova <svetlanashoronova@gmail.com>
2024-04-03 00:05:01 +0300

    task_1

---
+++ 2-nd course hw/hw_6.js
@@ -0,0 +1,7 @@
+// Задача 1 task
+
+const massiv1 = [ 1, 4, 5, 100, 10, 3];
+for (let index = 0; index < massiv1.length; index++) {
+     console.log(massiv1[index]);
+     if (massiv1[index] === 10) break;
+}
