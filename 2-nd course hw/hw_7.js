be563c4f5e4bf09d174f11929e7d66afa548f1f4
Svetlana Shoronova <svetlanashoronova@gmail.com>
2024-04-07 20:05:43 +0300

    task 2

--- 2-nd course hw/hw_7.js
+++ 2-nd course hw/hw_7.js
@@ -3,3 +3,11 @@
 let line = 'js';
 const lineUp = line.toUpperCase();
 console.log(lineUp);
+
+// Задача 2 task
+
+const lineMassiv2 = ['Солнце светит', 'Туча закрыла солнце', 'Солнечный свет', 'Белый лед', 'Солнце и лед' ];
+let line2 = 'СолНце';
+const NewLineMassiv2 = lineMassiv2.filter(lines => lines.toLowerCase().startsWith(line2.toLowerCase()) ) ;
+console.log(NewLineMassiv2);
+
