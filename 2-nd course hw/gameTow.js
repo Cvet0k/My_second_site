aef82dab36e5062911b7d6610d11433dcaae3ac7
Svetlana Shoronova <svetlanashoronova@gmail.com>
2024-04-09 00:56:09 +0300

    game Words

---
+++ 2-nd course hw/gameTow.js
@@ -0,0 +1,14 @@
+function gameWords () {
+const massivWords = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+massivWords.sort(()=> Math.round(Math.random()));
+alert(`Запони эти слова: \n${massivWords.join(' ')}`);
+let wordUser = prompt('Введи слова через пробел');
+if (massivWords.join(' ').toLowerCase() === wordUser.toLowerCase() ) {
+    alert('Ты угадал! Ты молодец')
+    
+} else { 
+    alert('Ты не угадал! Попробуй еще раз');
+    
+}
+}
+
