586d7425a0be06b7df557059ce6af28054f89bcf
Svetlana Shoronova <svetlanashoronova@gmail.com>
2024-04-16 00:36:07 +0300

    task 2

--- 2-nd course hw/hw_8.js
+++ 2-nd course hw/hw_8.js
@@ -4,17 +4,46 @@ const people = [
     { name: 'Анна', age: 17 },
     { name: 'Олег', age: 7 },
     { name: 'Оксана', age: 47 }
- ];
- function sortPeople  (person1, person2) { 
+];
+function sortPeople(person1, person2) {
     if (person1.age < person2.age) {
-        return -1 ;
+        return -1;
     } else {
         return 1;
     }
 }
- console.log(people.sort(sortPeople));
+console.log(people.sort(sortPeople));
 
- 
+// Task 2
+function isPositive(num) {
+    return num > 0;
+}
+
+function isMale(person2) {
+    return person2.gender === 'male';
+
+}
+function filter(arr, func) {
+    let result = [];
+    for (let index = 0; index < arr.length; index++) {
+        if (func(arr[index])) {
+            result.push(arr[index]);
+        }
+    }
+    return result;
 
 
- 
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people2 = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(people2, isMale));
+
+
